d7bd5532633bc61aca565a807addb765f5c17552
Dian956 <dianamwelu554@gmail.com>
2023-07-15 09:17:27 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,27 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-
 /**
- * main - Entry point
- * Returrn:0(success)
+ * main  - Entry point
+ * Discrption: "Check if root number is positive or negative"
+ * Return: Always 0 (success)
  */
-
-int main(void)
+int  main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is  positive\n", n);
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-	printf("%d is  positive\n", n);
+	{
+		printf("%d is zero\n", n);
+	}
 	else
-	printf("%d is  positive\n", n);
-	return  (0);
-}
-
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
