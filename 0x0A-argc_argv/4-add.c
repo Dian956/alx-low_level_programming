b62bb765289a26744e8ab64c17ce1ec20f73e753
Dian956 <dianamwelu554@gmail.com>
2023-08-06 23:32:44 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
-include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
 
 /**
  * main - Program that takes in all integer arguments and returns the sum
