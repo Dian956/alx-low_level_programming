f2d70bec4414d5c3aceb08a9b7f73f6926bc9548
Dian956 <dianamwelu554@gmail.com>
2023-08-10 04:20:47 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed.\n");
+		exit(98);
+	}
+
+	return (ptr);
+}
