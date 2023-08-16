b7e46adbc5136074508b26e22f06eac52f49d7b2
Dian956 <dianamwelu554@gmail.com>
2023-08-16 11:37:36 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,9 +13,9 @@
  * 99 on invalid operator
  * 100 on division by zero error
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num2;
+	int num1, num2;
 	char *op;
 
 	if (argc != 4)
@@ -24,23 +24,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-		if (strlen(op) != 1)
-		{
-			printf("Error\n");
-			 exit(99);
-		}
-
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	exit(0);
+	printf("%d\n", get_op_func(op)(num1, num2));
 
+	return (0);
 }
