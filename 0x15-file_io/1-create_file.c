970cec79a0656c03bb96b9c4f075a2192df953b5
Dian956 <dianamwelu554@gmail.com>
2023-09-04 13:01:43 -0700

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
+	for (len = 0; text_content[len]; len++)
+
 	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-		s = write(fd, text_content, len);
+		text_content = "";
+	s = write(fd, text_content, len);
+
 	if (s == -1)
 	{
 		return (-1);
