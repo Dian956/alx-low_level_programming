f3943ede62f6a91ced2a926ac695c3ff02d21e6d
Dian956 <dianamwelu554@gmail.com>
2023-09-04 11:32:46 -0700

    creating file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: content to write to the file
+ *
+ * Returns: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, s = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+	}
+		s = write(fd, text_content, len);
+	if (s == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
