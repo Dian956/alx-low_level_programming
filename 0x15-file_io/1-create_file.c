427a765326714097938bd68a4751d6041e0e4ac1
Dian956 <dianamwelu554@gmail.com>
2023-09-04 11:42:47 -0700

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,8 +7,7 @@
  * create_file - creates a file.
  * @filename: name of the file to create
  * @text_content: content to write to the file
- *
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -21,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		len = strlen(text_content);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 		s = write(fd, text_content, len);
 	if (s == -1)
