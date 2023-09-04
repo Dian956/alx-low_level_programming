dbc2e68f6234679cad2cd9060b8f311a99c13cb1
Dian956 <dianamwelu554@gmail.com>
2023-09-04 13:16:06 -0700

    creating list'

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,15 +19,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	for (len = 0; text_content[len]; len++)
 
 	if (text_content != NULL)
-		text_content = "";
-	s = write(fd, text_content, len);
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		s = write(fd, text_content, len);
 
 	if (s == -1)
 	{
+		close(fd);
 		return (-1);
+	}
 	}
 	close(fd);
 	return (1);
