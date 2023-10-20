4abc201550f2cfba050c849f37ff25fad12e6833
Dian956 <dianamwelu554@gmail.com>
2023-10-20 14:49:29 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints the given string to the std O
+ * @str: input pointer
+ * Return: returns void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
